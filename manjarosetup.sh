4037a6b4a23bb6ef0e31d5c89af925b6a9bff1a5
vfjpl <cosiekvfj@o2.pl>
2019-06-08 21:09:51 +0000

    Update manjarosetup.sh

--- manjarosetup.sh
+++ manjarosetup.sh
@@ -1,6 +1,10 @@
 pacman -r /mnt -Sy linux419 grub amd-ucode intel-ucode \
-bash coreutils nano sudo less \
+bash coreutils nano sudo less grep \
 pacman pamac-cli pamac-gtk pamac-tray-appindicator \
 lightdm networkmanager network-manager-applet pulseaudio pulseaudio-alsa \
+htop xfce4-terminal \
 firefox thunar mousepad \
-htop xfce4-terminal
+codeblocks
+
+fstabgen -U /mnt > /mnt/etc/fstab
+cp -a /mnt/etc/skel/. /mnt/home/kacper/
