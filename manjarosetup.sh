d842d5d89489547cf8627c029bc19496932ffc30
vfjpl <cosiekvfj@o2.pl>
2019-06-08 20:34:39 +0000

    Create manjarosetup.sh

---
+++ manjarosetup.sh
@@ -0,0 +1,6 @@
+pacman -r /mnt -Sy linux419 grub amd-ucode intel-ucode \
+bash coreutils nano sudo less \
+pacman pamac-cli pamac-gtk pamac-tray-appindicator \
+lightdm networkmanager network-manager-applet pulseaudio pulseaudio-alsa \
+firefox thunar mousepad \
+htop xfce4-terminal
