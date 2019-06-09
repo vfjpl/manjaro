494098e6972c1af52ff3aae8737b0a8cfbc361a4
vfjpl <cosiekvfj@o2.pl>
2019-06-09 17:16:42 +0200

    Update manjarosetup.sh

--- manjarosetup.sh
+++ manjarosetup.sh
@@ -1,25 +1,25 @@
-pacman --sysroot /mnt -Sy linux419 grub amd-ucode intel-ucode \
-`#Base`\
-bash coreutils diffutils e2fsprogs filesystem findutils \
-grep iputils less man-db man-pages nano pciutils shadow \
-sudo usbutils util-linux \
-`#Graphic and Audio`\
-mesa mesa-vdpau lib32-mesa lib32-mesa-vdpau \
-xorg-server xorg-xinit \
-pulseaudio pulseaudio-alsa alsa-utils \
-`#manjaro`\
-manjaro-xfce-minimal-settings \
-pacman pamac-cli pamac-gtk \
-lightdm lightdm-gtk-greeter \
-networkmanager network-manager-applet \
-manjaro-settings-manager \
-`#xfce`\
-xfwm4 xfdesktop xfce4-panel xfce4-session \
-xfce4-settings xfce4-whiskermenu-plugin \
-xfce4-terminal xfce4-screenshooter \
-thunar mousepad ristretto qpdfview \
-`#programs`\
-firefox htop vlc git \
-codeblocks hexchat
+basestrap -c /mnt "linux419 grub amd-ucode intel-ucode
+`#Base`
+bash coreutils diffutils e2fsprogs filesystem findutils
+grep iputils less man-db man-pages nano patch pciutils
+sed shadow sudo usbutils util-linux
+`#Graphic and Audio`
+mesa mesa-vdpau lib32-mesa lib32-mesa-vdpau
+xorg-server xf86-video-ati
+pulseaudio pulseaudio-alsa alsa-utils
+`#manjaro`
+manjaro-xfce-minimal-settings
+pacman pamac-cli pamac-gtk
+lightdm lightdm-gtk-greeter
+networkmanager network-manager-applet
+manjaro-settings-manager
+`#xfce`
+xfwm4 xfdesktop xfce4-panel xfce4-session
+xfce4-settings xfce4-whiskermenu-plugin
+xfce4-terminal xfce4-screenshooter
+thunar mousepad qpdfview viewnior
+`#programs`
+firefox htop vlc git
+codeblocks hexchat"
 
-manjaro-chroot /mnt "grub-install /dev/sda; update-grub"
+manjaro-chroot /mnt "grub-install /dev/sda"
