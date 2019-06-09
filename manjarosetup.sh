aeb254f268cd01d3e1757c2bdc92d4e8538c8a97
vfjpl <cosiekvfj@o2.pl>
2019-06-09 21:03:09 +0200

    Update manjarosetup.sh

--- manjarosetup.sh
+++ manjarosetup.sh
@@ -1,8 +1,8 @@
-basestrap -c /mnt "linux419 grub amd-ucode intel-ucode
+basestrap -cGiM /mnt "linux419 grub amd-ucode intel-ucode
 `#Base`
 bash coreutils diffutils e2fsprogs filesystem findutils
 grep iputils less man-db man-pages nano patch pciutils
-sed shadow sudo usbutils util-linux
+sed shadow sudo tlp usbutils util-linux
 `#Graphic and Audio`
 mesa mesa-vdpau lib32-mesa lib32-mesa-vdpau
 xorg-server xf86-video-ati
