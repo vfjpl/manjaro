4975465359109e65fce03d92cf66e1d7a64563f2
vfjpl <cosiekvfj@o2.pl>
2019-06-10 15:12:25 +0200

    Update manjarosetup.sh

--- manjarosetup.sh
+++ manjarosetup.sh
@@ -1,28 +1,28 @@
-basestrap -cGiM /mnt "linux419 grub amd-ucode intel-ucode
-`#Base`
-bash coreutils diffutils e2fsprogs filesystem findutils
-grep iputils less man-db man-pages nano patch pciutils
-sed shadow sudo systemd-sysvcompat tlp usbutils util-linux
-`#Graphic and Audio`
-mesa mesa-vdpau lib32-mesa lib32-mesa-vdpau
-xorg-server xf86-video-ati
-pulseaudio pulseaudio-alsa alsa-utils
-`#manjaro`
-manjaro-xfce-minimal-settings
-pacman pamac-cli pamac-gtk
-lightdm lightdm-gtk-greeter
-networkmanager network-manager-applet
-manjaro-settings-manager
-`#xfce`
-xfwm4 xfdesktop xfce4-panel xfce4-session
-xfce4-settings xfce4-whiskermenu-plugin
-xfce4-terminal xfce4-screenshooter
-xdg-user-dirs thunar thunar-archive-plugin
-mousepad qpdfview viewnior engrampa
-gvfs gksu-polkit catfish
-`#programs`
-firefox htop vlc git
-codeblocks hexchat streamlink"
+basestrap -cGiM /mnt linux419 grub amd-ucode intel-ucode \
+`#Base`\
+bash coreutils diffutils e2fsprogs filesystem findutils \
+grep iputils less man-db man-pages nano patch pciutils \
+sed shadow sudo systemd-sysvcompat tlp usbutils util-linux \
+`#Graphic and Audio`\
+mesa mesa-vdpau lib32-mesa lib32-mesa-vdpau \
+xorg-server xf86-video-ati \
+pulseaudio pulseaudio-alsa alsa-utils \
+`#manjaro`\
+manjaro-xfce-minimal-settings \
+pacman pamac-cli pamac-gtk \
+lightdm lightdm-gtk-greeter \
+networkmanager network-manager-applet \
+manjaro-settings-manager \
+`#xfce`\
+xfwm4 xfdesktop xfce4-panel xfce4-session \
+xfce4-settings xfce4-whiskermenu-plugin \
+xfce4-terminal xfce4-screenshooter \
+xdg-user-dirs thunar thunar-archive-plugin \
+mousepad qpdfview viewnior engrampa \
+gvfs gksu-polkit catfish \
+`#programs`\
+firefox htop vlc git \
+codeblocks hexchat streamlink
 
 #Install grub
 manjaro-chroot /mnt "grub-install /dev/sda"
