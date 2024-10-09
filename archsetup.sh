5fcd3bb7f051a146589a03cb0079928672bac107
vfjpl <cosiekvfj@o2.pl>
2024-10-09 12:50:55 +0200

    Update archsetup.sh

--- archsetup.sh
+++ archsetup.sh
@@ -1,25 +1,45 @@
 sudo pacman-mirrors -c Poland
 sudo pacman -Sy --noconfirm arch-install-scripts
 
-sudo pacstrap -Kic /mnt \
+sudo pacstrap -i /mnt \
+\
 grub update-grub amd-ucode intel-ucode \
-linux linux-firmware \
-base nano usbutils xssstate \
-base-devel cmake git \
+\
+linux-meta linux-firmware \
+\
+base nano openssh usbutils xssstate \
+\
+networkmanager network-manager-applet \
+\
+pulseaudio pulseaudio-alsa alsa-utils \
+\
 mesa lib32-mesa \
 mesa-vdpau lib32-mesa-vdpau \
 mesa-demos \
 xorg-server xf86-video-ati \
-pulseaudio pulseaudio-alsa alsa-utils \
-networkmanager network-manager-applet \
+\
+lightdm lightdm-gtk-greeter \
+\
+manjaro-xfce-minimal-settings \
+\
 firefox firefox-i18n-pl hunspell-pl hunspell-en_US \
-codeblocks
+codeblocks mpv \
+\
+base-devel cmake git \
+sfml poco boost lua51 physfs
 
 genfstab -U /mnt | sudo tee -a /mnt/etc/fstab
 sudo sed -i '/pl_PL.UTF-8/s/#//' /mnt/etc/locale.gen
 echo "LANG=pl_PL.UTF-8" | sudo tee /mnt/etc/locale.conf
 echo "KEYMAP=pl" | sudo tee /mnt/etc/vconsole.conf
 sudo sed -i '/%wheel ALL=(ALL:ALL) ALL/s/# //' /mnt/etc/sudoers
+sudo sed -i '/Inherits/s/Adwaita//' /mnt/usr/share/icons/default/index.theme
+echo "Section \"InputClass\"
+	Identifier \"system-mouse\"
+	Driver \"libinput\"
+	MatchIsPointer \"yes\"
+	Option \"AccelProfile\" \"flat\"
+EndSection" | sudo tee /mnt/etc/X11/xorg.conf.d/50-mouse.conf
 
 arch-chroot /mnt "ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime"
 arch-chroot /mnt "locale-gen"
@@ -27,8 +47,9 @@ arch-chroot /mnt "grub-install --target=i386-pc /dev/sda"
 arch-chroot /mnt "update-grub"
 arch-chroot /mnt "useradd cosiek -m -G wheel,storage,input,video,audio,power,optical,network,lp,scanner,sys"
 arch-chroot /mnt "useradd vfjpl -m -G wheel,storage,input,video,audio,power,optical,network,lp,scanner,sys"
+arch-chroot /mnt "systemctl enable lightdm NetworkManager systemd-timesyncd"
 
-echo "Haslo cosiek:"
+echo "Password cosiek:"
 arch-chroot /mnt "passwd cosiek"
-echo "haslo vfjpl:"
+echo "Password vfjpl:"
 arch-chroot /mnt "passwd vfjpl"
