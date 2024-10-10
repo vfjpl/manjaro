d93a1b8ed417653e54418e39876890c115e50bd4
vfjpl <cosiekvfj@o2.pl>
2024-10-10 14:50:15 +0000

    fix sed command

--- archsetup.sh
+++ archsetup.sh
@@ -33,10 +33,10 @@ echo "LANG=pl_PL.UTF-8" | sudo tee /mnt/etc/locale.conf
 echo "KEYMAP=pl" | sudo tee /mnt/etc/vconsole.conf
 sudo sed -i '/%wheel ALL=(ALL:ALL) ALL/s/# //' /mnt/etc/sudoers
 sudo sed -i '/Inherits/s/Adwaita//' /mnt/usr/share/icons/default/index.theme
-sudo sed -i 'DISK_DEVICES/s/nvme0n1 sda/sda sdb/' /mnt/etc/tlp.conf
-sudo sed -i 'DISK_IOSCHED/s/mq-deadline mq-deadline/bfq bfq/' /mnt/etc/tlp.conf
-sudo sed -i 'DISK_DEVICES/s/#//' /mnt/etc/tlp.conf
-sudo sed -i 'DISK_IOSCHED/s/#//' /mnt/etc/tlp.conf
+sudo sed -i '/DISK_DEVICES/s/nvme0n1 sda/sda sdb/' /mnt/etc/tlp.conf
+sudo sed -i '/DISK_IOSCHED/s/mq-deadline mq-deadline/bfq bfq/' /mnt/etc/tlp.conf
+sudo sed -i '/DISK_DEVICES/s/#//' /mnt/etc/tlp.conf
+sudo sed -i '/DISK_IOSCHED/s/#//' /mnt/etc/tlp.conf
 
 echo "set-card-profile alsa_card.pci-0000_06_00.4 output:analog-stereo+input:analog-stereo
 set-sink-port alsa_output.pci-0000_06_00.4.analog-stereo analog-output-lineout" | sudo tee /etc/pulse/default.pa.d/card-profile-sink-port.pa
