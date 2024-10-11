cec773c7adc60721c9321785bfc4f3b4181dfb82
vfjpl <cosiekvfj@o2.pl>
2024-10-11 12:22:13 +0200

    Update archsetup.sh

--- archsetup.sh
+++ archsetup.sh
@@ -18,7 +18,7 @@ xfwm4 xfconf xfdesktop \
 xfce4-session xfce4-panel xfce4-whiskermenu-plugin \
 xfce4-settings xfce4-terminal xfce4-screenshooter \
 thunar thunar-archive-plugin thunar-media-tags-plugin \
-mousepad \
+mousepad xfce4-pulseaudio-plugin pavucontrol \
 xdg-user-dirs gvfs \
 firefox firefox-i18n-pl hunspell-pl hunspell-en_US \
 codeblocks mpv htop \
